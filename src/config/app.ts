c165b55b1fd32c4f5f1e14ba061da6a0e010b037
Rojan Rajbhandari <rrojan20@gmail.com>
2024-03-17 12:57:09 +0545

    feat: implement sentry for error monitoring

--- src/config/app.ts
+++ src/config/app.ts
@@ -2,6 +2,9 @@ const appConfig = {
   copilotApiKey: process.env.COPILOT_API_KEY || '',
   webhookSigningSecret: process.env.WEBHOOK_SIGNING_SECRET || '',
   apiUrl: `${process.env.VERCEL_ENV === 'development' ? 'http://' : 'https://'}${process.env.VERCEL_URL}`,
+  sentry: {
+    DSN: process.env.NEXT_PUBLIC_SENTRY_DSN || '',
+  },
 };
 
 export default appConfig;
